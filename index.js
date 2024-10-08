6b085f1db27d7da08c69fbd0c933cc75b5a0a161
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-10-08 12:49:38 +0300

    Scope variables

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+window.customerName = "bob"
+ 
+
+function upperCaseCustomerName(){
+    window.customerName = window.customerName.toUpperCase();
+
+}
+
+function setBestCustomer(){
+    window.bestCustomer = "not bob"
+
+}
+
+function overwriteBestCustomer(){
+    window.bestCustomer = "maybe bob"
+}
+
+const leastFavourite = "Miguel"
+function changeLeastFavoriteCustomer(){
+    leastFavourite= "Andy"
+
+}
